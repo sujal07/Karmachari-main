e7d1231747e6eb9c74f6204a9897c483b68aa036
Sujal Maharjan <sujalmhrzn07@gmail.com>
2023-02-03 12:32:31 +0545

    Login Logout UserInfo dislay

--- account/login/urls.py
+++ account/login/urls.py
@@ -1,9 +1,15 @@
 
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
     path('login',views.login),
     path('signup',views.signup),
+    path('logout',views.logout,name='logout') 
 ]
+
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
--- account/login/views.py
+++ account/login/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User,auth
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-def home(request):
-    return render(request,'Home.html')
+@login_required(login_url='/login')
+def home(request): 
+    # current_user=request.user
+    # user_id='current_user_id'
+    # print(user_id)
+    # context={
+    #     'user_id':user_id
+    # }
+    fullname =  request.user.get_full_name()
+    context = {'fullname':fullname}
+    return render(request,'Home.html',context)
 
-def login(request):
-    return render(request,'login.html')
 
 def signup(request):
     return render(request,'signup.html')
@@ -23,3 +31,11 @@ def login(request):
             return redirect('/login')
     else: 
         return render(request,'login.html')
+    
+    
+@login_required(login_url='/login')
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('/login')
+
